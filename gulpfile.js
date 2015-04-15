cd63ba44a6f933795c313fe60f87eeafddb29bd5
Kyle Halterman <halterman.kyle@gmail.com>
2015-04-15 07:38:54 -0700

    Added coffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 //bring in the gulp library and assign it to the variable gulp
 //all the methods and properties assigned to this var
-var gulp = require('gulp');
-	gutil = require('gulp-util');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
+										//*.coffee any file with extension of coffee
+var coffeeSources = ['components/coffee/tagline.coffee']
 
-gulp.task('log', function(){
-	gutil.log('Workflows are awesome');
+gulp.task('coffee', function() {
+	gulp.src('coffeeSources')
+	//bare true compiles j without putting in safety wrapper
+	.pipe(coffee({ bare: true })
+		//use gutil plugin and send error as log to console
+		.on('error', gutil.log))
+	//move processed file to scripts folder
+	.pipe(gulp.dest('components/scripts'))
 });
