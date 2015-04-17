e82fbcb825ca1e4ff4b50f6938ce4ee4aa1ff8db
Kyle Halterman <halterman.kyle@gmail.com>
2015-04-16 20:28:55 -0700

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
 	concat = require('gulp-concat');
 
 										//*.coffee any file with extension of coffee
@@ -26,5 +27,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
