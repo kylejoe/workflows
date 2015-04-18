30c4a38a69fa002e8c22eefcf12f5581443334f8
Kyle Halterman <halterman.kyle@gmail.com>
2015-04-17 21:34:21 -0700

    added gulp watch commandgit status Sweet

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,53 @@
 //bring in the gulp library and assign it to the variable gulp
 //all the methods and properties assigned to this var
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee'),
-	browserify = require('gulp-browserify'),
-	compass = require('gulp-compass'),
-	concat = require('gulp-concat');
-
-										//*.coffee any file with extension of coffee
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat');
+    				//*.coffee any file with extension of coffee
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
-		'components/scripts/pixgrid.js',
-		'components/scripts/rclick.js',
-		'components/scripts/tagline.js',
-		'components/scripts/template.js'
-		];
-
-var sassSources = ['components/sass/style.scss'];	
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
-	gulp.src('coffeeSources')
-	//bare true compiles j without putting in safety wrapper
-	.pipe(coffee({ bare: true })
-		//use gutil plugin and send error as log to console
-		.on('error', gutil.log))
-	//move processed file to scripts folder
-	.pipe(gulp.dest('components/scripts'))
+  gulp.src(coffeeSources)
+  //bare true compiles j without putting in safety wrapper
+    .pipe(coffee({ bare: true })
+    	//use gutil plugin and send error as log to console
+      .on('error', gutil.log))
+    //move processed file to scripts folder
+    .pipe(gulp.dest('components/scripts'))
 });
-				//this coffee array will run before js
+
 gulp.task('js', function() {
-	gulp.src(jsSources)
-		.pipe(concat('script.js'))
-		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'))
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest('builds/development/js'))
 });
 
 gulp.task('compass', function() {
-	gulp.src(sassSources)
-		.pipe(compass({
-			sass: 'components/sass',
-			image: 'builds/development/images',
-			style: 'expanded'
-		})
-		//output something to the console if an error
-		.on('error', gutil.log))
-		.pipe(gulp.dest('builds/development/css'))
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
 });
 
-			//when default is run it will run all these tasks
-			// when just gulp is called
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('watch', function() {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
